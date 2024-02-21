9d290b42ac6af364eb7824eef77c1a4ac363b588
JuliaPontes <juliapcp2003@gmail.com>
2024-02-21 20:52:31 -0300

    Desenvolvida busca por texto

--- src/app/componentes/pensamentos/listar-pensamento/listar-pensamento.component.ts
+++ src/app/componentes/pensamentos/listar-pensamento/listar-pensamento.component.ts
@@ -12,20 +12,29 @@ export class ListarPensamentoComponent implements OnInit {
   listaPensamentos: Pensamento[] = [];
   paginaAtual: number = 1;
   haMaisPensamentos: boolean = true;
+  filtro: string = 'queen';
   constructor(private service: PensamentoService) { }
 
   ngOnInit(): void {
-    this.service.listar(this.paginaAtual).subscribe((listaPensamentos) => {
+    this.service.listar(this.paginaAtual, this.filtro).subscribe((listaPensamentos) => {
       this.listaPensamentos = listaPensamentos
     })
   }
 
   carregarMaisPensamentos() {
-    this.service.listar(++this.paginaAtual).subscribe((listaPensamentosRetorno) => {
+    this.service.listar(++this.paginaAtual, this.filtro).subscribe((listaPensamentosRetorno) => {
       this.listaPensamentos.push(...listaPensamentosRetorno);
       if (!listaPensamentosRetorno.length) {
         this.haMaisPensamentos = false;
       }
+    })
+  }
+
+  pesquisarPensamentos(){
+    this.haMaisPensamentos = true;
+    this.paginaAtual = 1;
+    this.service.listar(this.paginaAtual, this.filtro).subscribe((listaPensamentos) => {
+      this.listaPensamentos = listaPensamentos;
     })
   }
 }
